48c2758e23edce6a5a902ffed7b9a7ed42b2736c
unknown <kayy4>
2023-03-26 21:29:48 +0530

    Linked List Add Node in the begining

--- LinkedList/InsertingNodeBegining.cpp
+++ LinkedList/InsertingNodeBegining.cpp
@@ -7,15 +7,21 @@ struct Node {
 struct Node* HeadNode;
 
 void Insert(int values){   //insert function
-  //creating a node
+  //First create a node then Inserting a node at the bigining 
   Node* Temp1 = (Node*)malloc(sizeof(Node));
   (*Temp1).Data = values;
-  (*Temp1).Link = NULL;
-  HeadNode = Temp1;        //Headnode will store the Node's Address
+  (*Temp1).Link = HeadNode; // New node Link is Linked to previous Node,If the List is Empty-->Headnode is NULL. 
+  HeadNode = Temp1;         // Store the new node (first Node) Address in "Headnode" 
 }
 
 int Print(){
-
+  struct Node* Temp1 = HeadNode;
+  printf("List is:");
+  while(Temp1 != NULL){
+    printf(" %d", (*Temp1).Data);
+    Temp1= (*Temp1).Link; 
+  }
+  printf("\n");
 }
 
 int main(){
@@ -23,13 +29,11 @@ int main(){
   //storing Data into the LinkedList 
   printf ("How Many Numbers To Store?\n");
   int n, x;
-  scanf("%d", &n);
+  scanf("%d, ", &n);
   for(int i=0; i<=n; i++){
     scanf("%d", &x);
     Insert(x);    //function to insert
     Print();      //function to print the List
   }
-
-
 }
 
